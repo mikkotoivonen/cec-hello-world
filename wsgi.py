2d09efa0d5ccb08759e1bac541f4948589f199ca
Mikko Toivonen <mikko.e.toivonen@helsinki.fi>
2018-02-16 09:13:03 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     application.run()
     hostname = socket.gethostname()
     timestamp = str(datetime.now())
-    file = open(“/mnt/log”,”w”)
+    file = open(“/mnt/log”,”a”)
     file.write(timestamp + ":" + hostname +  "\n")
     file.close()
