23767bb6716a6bd4ef155dee015e0ed43a127465
Mikko Toivonen <mikko.e.toivonen@helsinki.fi>
2018-02-16 09:01:50 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,22 @@
 import socket
 from flask import Flask
+from datetime import datetime
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     
-    return "Hello World! Greetings from "+socket.gethostname()+"\n"
+    file = open(“/mnt/log”,”r”)
+    out = file.read()
+    file.close()
+    return out
 
 
 if __name__ == "__main__":
     application.run()
+    hostname = socket.gethostname()
+    timestamp = str(datetime.now())
+    file = open(“/mnt/log”,”w”)
+    file.write(timestamp + ":" + hostname +  "\n")
+    file.close()
